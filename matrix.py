7030df86dfe2ed15aed5a287dd7730c5a1457e20
Thomas de la Gorgue de Rosny <159559547+lebabouinvolant@users.noreply.github.com>
2026-08-23 02:10:17 +0200

    Added real backpropagation learning

--- matrix.py
+++ matrix.py
@@ -1,3 +1,5 @@
+import random
+
 class Matrix():
     def __init__(self, PARAMETER): 
         """Parameter peut être soit un tuple de 2 nombres (le nb de lignes et de colonnes de la matrice) soit un tableau de nombres à 2 dimensions qui sert de modèle"""
@@ -11,14 +13,14 @@ class Matrix():
             self.representation = [[0 for _ in range(self.columns)] for _ in range(self.lines)]
         else:
             raise Exception("L'argument passé à la méthode d'initialisation n'est pas correct (tuple de 2 éléments ou tableau 2D)")
-    def multiply(self, OTHER):
+    def multiply(self, OTHER) -> Matrix:
         """Cette fonction renvoie le produit d'une matrice par un scalaire ou une autre matrice
         Elle prend en argument soit un nombre, auquel cas la matrice sera multipliée par un scalaire, soit une matrice, auquel cas on fera un produit matriciel"""
         if type(OTHER) == Matrix:
             return self.multiplybymatrix(OTHER)
         else:
             return self.multiplybyscalar(OTHER)
-    def multiplybymatrix(self, otherMatrix: Matrix):
+    def multiplybymatrix(self, otherMatrix: Matrix) -> Matrix:
         """Fonction renvoyant le produit de 2 matrices et prenant en argument la matrice à multiplier avec l'instance de classe"""
         if self.columns != otherMatrix.lines:
             raise Exception("Les 2 matrices ne sont pas compatibles pour un produit") #Multiplication impossible
@@ -30,22 +32,23 @@ class Matrix():
                     val += self.representation[i][k] * otherMatrix.representation[k][j]
                 output[i][j] = val
         return output
-    def multiplybyscalar(self, scalar):
+    def multiplybyscalar(self, scalar) -> Matrix:
         output = Matrix([[0 for _ in range(self.columns)] for _ in range(self.lines)])
         for i in range(self.lines):
             for j in range(self.columns):
                 output.representation[i][j] = self.representation[i][j] * scalar
         return output
-    def sum(self, otherMatrix: Matrix):
+    def sum(self, otherMatrix: Matrix) -> Matrix:
         """Somme de 2 matrices de même dimension"""
         if self.columns != otherMatrix.columns or self.lines != otherMatrix.lines:
             raise Exception("Les 2 matrices ne sont pas compatibles pour une somme")
-        output = Matrix([[0 for _ in range(self.columns)] for _ in range(self.lines)])
+        output = Matrix((self.lines, self.columns))
         for i in range(self.lines):
             for j in range(self.columns):
-                output = self.representation[i][j] + otherMatrix[i][j]
+                output.representation[i][j] = self.representation[i][j] + otherMatrix[i][j]
+        
         return output
-    def sub(self, otherMatrix: Matrix):
+    def sub(self, otherMatrix: Matrix) -> Matrix:
         """Somme de 2 matrices de même dimension"""
         if self.columns != otherMatrix.columns or self.lines != otherMatrix.lines:
             raise Exception("Les 2 matrices ne sont pas compatibles pour une somme")
@@ -54,6 +57,10 @@ class Matrix():
             for j in range(self.columns):
                 output = self.representation[i][j] - otherMatrix[i][j]
         return output
+    def randomize(self) -> None:
+        for i in range(self.lines):
+            for j in range(self.columns):
+                self.representation[i][j] = (random.random()-0.5) * 2 
 
     def assign(self, target: list):
         """Cette méthode permet d'assigner la matrice à une nouvelle représentation"""
@@ -65,22 +72,34 @@ class Matrix():
         for i in range(self.lines):
             for j in range(self.columns):
                 output[j][i] = self.representation[j][i]
-    def __add__(self, other: Matrix):
+    def map(self, func: function):
+        for el in self.representation:
+            for el2 in el:
+                el2 = func(el2)
+    def bilan(self):
+        out = 0
+        for el in self.representation:
+            for el2 in el:
+                out += el2
+        return out
+    def __add__(self, other: Matrix) -> Matrix:
         """Somme de 2 matrices de même dimension"""
         return self.sum(other)
-    def __sub__(self, other: Matrix):
+    def __sub__(self, other: Matrix) -> Matrix:
         """Différence de 2 matrices de même dimension"""
         return self.sub(other)
     def __getitem__(self, key):
         """Récupérer les valeurs contenues dans la matrices (utilise l'attribut sous jacent representation)"""
         return self.representation[key]
-    def __mul__(self, other):
+    def __mul__(self, other) -> Matrix:
         """Multiplie une matrice soit par un scalaire soit par une autre matrice compatible"""
         return self.multiply(other)
-    def __repr__(self):
+    def __repr__(self) -> str:
         rep = ""
         for el in self.representation:
             rep += str(el) + "\n"
         return rep
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.representation)
+    def __len__(self) -> int:
+        return len(self.representation)
--- neural-network.py
+++ neural-network.py
@@ -1,28 +1,84 @@
 import random
 from matrix import Matrix
+import math
 class NeuralNetwork():
     """La classe de base pour un réseau de neurone, la fonction d'initialisation prend les nombres d'inputs, de neurones cachés et d'outputs en entrée
     Pour l'instant c'est un réseau 3 couches et fully connected, on passera au deep learning plus tard
     On initialise les poids entre -1 et 1"""
-    def __init__(self, ninputs, nhiddens, noutputs):
-        """L'initialisation de la classe NeuralNetwork prend 3 arguments: le nombre d'entrées du réseau, le nombre de neurones de la c  """
+    def __init__(self, ninputs, nhiddens, noutputs, nhiddenlayers = 1):
+        """L'initialisation de la classe NeuralNetwork prend 3 arguments: le nombre d'entrées du réseau, le nombre de neurones de la couche cachée et le nombre de sorties  """
         self.inputnumber = ninputs
-        self.hiddens = Matrix([
-            [(random.random()-0.5) * 2 for _ in range(ninputs)] for _ in range(nhiddens)
-        ])
-
-        self.outputs = Matrix([[(random.random()-0.5) * 2 for _ in range(nhiddens)] for _ in range(noutputs)])
+        self.neurons = [Matrix((nhiddens, ninputs))] #self.neurons[couche][neurone][poids(autrement dit index du précédent neurone qui est connecté par ce poids)] La première hidden layer doit prendre les inputs en entrée, contrairement aux suivantes
+        for _ in range(nhiddenlayers-1):
+            self.neurons.append(Matrix((nhiddens, nhiddens)))
+        self.neurons.append(Matrix((noutputs, nhiddens))) # The output layer
+        self.biases = [Matrix((nhiddens, 1)) for _ in range(nhiddenlayers)]
+        self.biases.append(Matrix((noutputs, 1))) #Output bias
+        for el in self.neurons:
+            el.randomize()
+        for el in self.biases:
+            el.randomize()
     def feedForward(self, inputs: Matrix):
         """
         La fonction responsable de faire fonctionner le réseau de neurone.
         Elle prend un argument: les inputs (sous forme d'une matrice de n*1)
         """
-        processed = self.hiddens * inputs
-        output = self.outputs * processed
-        return output
-    def activate():
-        pass
-        
+        processed = inputs
+        for i in range(len(self.neurons)):
+            processed = self.neurons[i] * processed
+            processed += self.biases[i]
+            processed.map(self.sigmoid) #Activation
+        return processed
+    def feedForTrain(self, inputs : Matrix):
+        """
+        Similaire à feedForward sauf qu'elle renvoie aussi les outputs intermédiaires
+        """
+        processed = inputs
+        intermediates = []
+        for i in range(len(self.neurons)):
+            processed = self.neurons[i] * processed
+            processed += self.biases[i]
+            intermediates.append([processed]) #The actual values
+            processed.map(self.sigmoid) #Activation
+            intermediates[i].append(processed) #Activated values
+        return processed, intermediates
 
-test = NeuralNetwork(1, 5, 3)
-print(test.feedForward(Matrix([[1]])))
+    def train(self, input, answer):
+        """
+        La fonction responsable d'entraîner le réseau de neurone avec un input et un output donné.
+        Les inputs et les outputs doivent être donnés sous forme de matrices (de la même taille que prévus par le réseau, cela va de soi)
+        """
+        result, intermediates = self.feedForTrain(input)
+        errors = [(result[i][0]-answer[i][0])**2 for i in range(len(result))]
+        lastlayer = len(self.neurons)-1
+        cost = sum(errors)/len(errors) #Le coût est la moyenne des erreurs
+        deltas = [[0 for _ in range(len(self.neurons[i]))] for i in range(len(self.neurons))] #On y stocke les dérivées des neurones (somme des connections): 1 élément par neurone, cela fait aussi office de gradient du biais
+        weightgradient = [[[0 for _ in range(len(self.neurons[i][j]))] for j in range(len(self.neurons[i]))] for i in range(len(self.neurons))] #On y stocke les dérivées des poids (1 élément par connection)
+        for i in range(len(self.neurons[lastlayer])): #Pour chaque neurone de la dernière couche
+            outputderivated = (2 / len(self.neurons[lastlayer])) * (intermediates[lastlayer][i][1] - answer[i][0]) #A quel point changer l'output va changer le coût (qui est égal à la moyenne des erreurs)
+            activationderivated = intermediates[lastlayer][i][1] * (1-intermediates[lastlayer][i][1]) #Formule pour la fonction sigmoïde
+            deltas[lastlayer][i] = outputderivated * activationderivated
+            for j in range((len(self.neurons[lastlayer][i]))): #Pour chaque poids
+                weightgradient[lastlayer][i][j] = deltas[lastlayer][i] * intermediates[lastlayer-1][j][1] #delta de ce neurone fois activation de celui auquel relie le poids
+        for i in range(len(self.neurons)-2, -1, -1): #Pour chaque couche en descendant (sauf la couche d'output)
+            for j in range(len(self.neurons[i])): #Pour chaque neurone
+                errorDerivated = 0
+                for neuronIndex in range(len(deltas[i+1])): #On somme tous les deltas des neurones suivants fois les poids qui les relient au neurone actuel pour trouver le gradient d'activation du neurone actuel par rapport à la valeur activée du neurone
+                    errorDerivated += deltas[i+1][neuronIndex] * self.neurons[i+1][neuronIndex][j]
+                deltas[i][j] = intermediates[i][j][1] * (1-intermediates[i][j][1]) * errorDerivated
+                for k in range(len(self.neurons[i][j])): #Pour chaque connection appartenant à un neurone
+                    if i == 0: #On doit aller chercher non pas la valeur activée du neurone précédent mais l'input
+                        weightgradient[i][j][k] = deltas[i][j] * input[k][0]
+                    else: weightgradient[i][j][k] = deltas[i][j] * intermediates[i-1][k][1]
+        for i in range(len(self.neurons)):
+            for j in range(len(self.neurons[i])):
+                for k in range(len(self.neurons[i][j])):
+                    self.neurons[i][j][k] = weightgradient[i][j][k] #Update weights
+                self.biases[i][j] = deltas[i][j] #Update biases
+    @staticmethod 
+    def sigmoid(x):
+        return 1/(1+math.e**-x)
+    
+
+test = NeuralNetwork(1, 5, 3, 3)
+print(test.feedForTrain(Matrix([[1]])))
